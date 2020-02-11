d748b92fabb1a91d1a0b10b3d605ea9fb2b4c8fd
rchurchASC <rahmelchurch@gmail.com>
2020-02-11 23:10:40 +0000

    Start

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+$("#search").click(function(){
+    let userInput = $("#userInput").val();
+    fetch("https://api.giphy.com/v1/gifs/search?q=" + userInput + "&rating=pg&api_key=dc6zaTOxFJmzC")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        });
+        
+});
